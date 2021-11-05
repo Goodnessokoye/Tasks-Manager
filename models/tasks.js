e42b781efe5272d20391e4c587d72c5f41f9df98
Goodness <goodnessokoye10@gmail.com>
2021-11-05 15:31:20 +0100

    added models

--- models/tasks.js
+++ models/tasks.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const taskSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    trim: true,
+    required: [true, "Name is required"],
   },
 
   completed: {
